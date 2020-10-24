66223d65043e9c6643b51e3cc93b6ac05ac39fd8
Brenda Profiro <brendaprofiro@gmail.com>
2020-10-24 18:04:13 -0300

    added: criar reducers para faiulre e habilitar no component

--- src/components/CatalogItem.tsx
+++ src/components/CatalogItem.tsx
@@ -1,6 +1,7 @@
 import React, { useCallback } from 'react';
-import { useDispatch } from 'react-redux';
-import { addProductToCart } from '../store/modules/cart/actions';
+import { useDispatch, useSelector } from 'react-redux';
+import { IState } from '../store';
+import { addProductToCartRequest } from '../store/modules/cart/actions';
 import { IProduct } from '../store/modules/cart/type';
 
 
@@ -11,8 +12,12 @@ interface CatalogItemProps {
 const CatalogItem: React.FC<CatalogItemProps> = ({ product }) => {
   const dispatch = useDispatch();
 
+  const hasFailedStockCheck = useSelector<IState, boolean>((state: IState) => {
+    return state.cart.failedStockCheck.includes(product.id)
+  })
+
   const handleAddProductToCart = useCallback(() => {
-    dispatch(addProductToCart(product));
+    dispatch(addProductToCartRequest(product));
   }, [dispatch, product]);
 
   return (
@@ -26,6 +31,8 @@ const CatalogItem: React.FC<CatalogItemProps> = ({ product }) => {
     >
       Comprar
     </button>
+
+    { hasFailedStockCheck && <span style={{ color: 'red' }} >Falta de estoque</span>}
   </article>
   );
 }
